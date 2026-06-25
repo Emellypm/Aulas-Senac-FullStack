bfb9740005f1de10af29c6495ff94afc0a0169df
Emelly Moraes <moraes.emelly@icloud.com>
2026-06-25 15:58:20 -0300

    25-06

--- Back/backend/scr/services/usuarios/UsuariosServices.ts
+++ Back/backend/scr/services/usuarios/UsuariosServices.ts
@@ -1,12 +1,24 @@
+import prismaClient from "../../prisma/PrismaClient"
+
 interface cadUsuarios {
-    nome: String,
-    email: String,
-    senha: String,
-    telefone: String,
+    nome: string,
+    email: string,
+    senha: string,
+    telefone: string,
 }
 
 class UsuariosServices {
     async cadastrarUsuarios({ nome, email, senha, telefone }: cadUsuarios) {
+
+        await prismaClient.usuarios.create({
+            data: {
+                nome: nome,
+                email: email,
+                senha: senha,
+                telefone: telefone
+            }
+        })
+
         return ({ dados: 'Dados salvos com sucesso' })
     }
 }
