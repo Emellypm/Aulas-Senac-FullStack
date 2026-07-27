987b496c8fa896dd8192297e7a67fb20cc6ed13c
Emelly Moraes <moraes.emelly@icloud.com>
2026-07-27 17:18:58 -0300

    27-07 surto

--- Front/TechStore/TechStore/src/componentes/Hero.jsx
+++ Front/TechStore/TechStore/src/componentes/Hero.jsx
@@ -30,6 +30,7 @@ function Hero() {
                 />
 
             </div>
+
         </section>
     )
 }
---
+++ Front/aula_13/aula-13/src/App.jsx
@@ -0,0 +1,12 @@
+import Carrossel from "./Componentes/Carrossel";
+import GuiaCarrossel from "./Componentes/GuiaCarrossel";
+
+function App() {
+  return (
+    <>
+      <Carrossel />
+      <GuiaCarrossel />
+    </>
+  );
+}
+export default App;
